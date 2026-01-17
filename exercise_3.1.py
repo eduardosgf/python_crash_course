ab212ab0f885c7ecd581878bc03226bb2ffa5b3f
Eduardo Silva <132854894+eduardosgf@users.noreply.github.com>
2026-01-17 21:22:57 +0000

    Exercises 3.1 - 3.3

---
+++ exercise_3.1.py
@@ -0,0 +1,9 @@
+# Exercise 3.1
+names = ['Rebeca', 'Eduardo', 'Jose', 'Bruno', 'Brenda', 'Fabiana']
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
---
+++ exercise_3.3.py
@@ -0,0 +1,10 @@
+# Exercise 3.3
+
+veiculos = ['moto', 'carro', 'aviao']
+message = "Gosto de andar de"
+
+print(f"{message} {veiculos[0]}")
+
+print(f"{message} {veiculos[1]}")
+
+print(f"{message} {veiculos[2]}")
