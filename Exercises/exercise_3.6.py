53a922bd721f68dc137705fa05116c2f5b651d07
Eduardo Silva <132854894+eduardosgf@users.noreply.github.com>
2026-01-18 05:14:24 +0000

    2 novos exercisios adicionados

---
+++ Exercises/exercise_3.6.py
@@ -0,0 +1,38 @@
+# Exercise 3.5
+
+pessoas = ['Rebeca', 'José', 'Fabiana']
+message = "Quer jantar comigo"
+
+# Lista Original
+print("Convite Original:", pessoas) 
+
+# Convite para lista original
+print(f"{message} {pessoas[0].title()} ?")
+print(f"{message} {pessoas[1].title()} ?")
+print(f"{message} {pessoas[2].title()} ?")
+
+# Remove alguem
+nao_vai = "Nao vai ir ao jantar"
+desistiu = pessoas.pop()
+
+print(f"{desistiu.title()} {nao_vai}")
+
+# Adiciona uma pessoa
+pessoas.insert(0, 'Brenda')
+
+#Convite lista modificada
+print("Convite modificada:", pessoas)
+print(f"{message} {pessoas[0].title()} ?")
+print(f"{message} {pessoas[1].title()} ?")
+print(f"{message} {pessoas[2]} ?")
+
+print("Encontramos uma mesa maior para o jantar")
+
+p1 = pessoas.insert(0, 'Bruno')
+pessoas.insert(2, 'Cleusita')
+pessoas.append('Ovidio')
+
+print("Convidados adicionado a lista:", pessoas)
+
+for pessoa in pessoas:
+    print(f"{message} {pessoa.title()} ?")
